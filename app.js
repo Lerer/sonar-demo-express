f196215bf90b53bddf171abf90c41a731de86ce0
Yaakov (Coby) Lerer <coby_lerer@hotmail.com>
2026-05-19 21:01:16 +1000

    remove comment

--- app.js
+++ app.js
@@ -227,7 +227,6 @@ app.get('/search-safe', (req, res) => {
 app.get('/transform/:text', (req, res) => {
   let input = req.params.text;
   
-  // Sonar Issue: Duplicated if-else block (also appears in /convert route below)
   if (input === 'admin') {
     input = 'user';
   } else if (input === 'root') {
@@ -247,7 +246,6 @@ app.get('/transform/:text', (req, res) => {
 app.get('/convert/:text', (req, res) => {
   let input = req.params.text;
   
-  // Sonar Issue: EXACT DUPLICATE of the if-else block from /transform route
   if (input === 'admin') {
     input = 'user';
   } else if (input === 'root') {
